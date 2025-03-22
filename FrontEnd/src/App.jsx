9a876819face989473a8d43ec331edc3dd2285c0
Arifkhan2007111 <arifkhan2007111@gmail.com>
2025-03-22 16:44:38 +0530

    Some changes

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 
 import Landing from './components/landing'
+import Ranking from './components/RankingPage'
+import FilterPanel from './components/FilterPanel'
 
 function App() {
 
   return (
     <>
-      <Landing />
+      <FilterPanel />
     </>
   )
 }
