7d1d0ed8d6bfd836dae7a0264eeba5ca18e90f45
Arifkhan2007111 <arifkhan2007111@gmail.com>
2025-03-22 23:18:54 +0530

    Some Changes

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,12 +4,15 @@ import './App.css'
 import Landing from './components/landing'
 import Ranking from './components/RankingPage'
 import FilterPanel from './components/FilterPanel'
+import EngagementChart from './components/EngagementChart'
+import Profile from './components/UserProfile'
+import Admin from './components/AdminPanel'
 
 function App() {
 
   return (
     <>
-      <FilterPanel />
+      <Ranking />
     </>
   )
 }
