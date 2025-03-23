9586a16936e7dfd8bb054cbb9243e0cc80dbd9ba
TheLunarDev <153661521+Mayank2177@users.noreply.github.com>
2025-03-23 23:23:33 +0530

    Update Landing.py

--- Backend/Landing.py
+++ Backend/Landing.py
@@ -1,1 +1,14 @@
+from flask import Flask, render_template, redirect, url_for
 
+app = Flask(__name__)
+
+@app.route('/')
+def landing_page():
+    return render_template('landing.html')
+
+@app.route('/search')
+def search_page():
+    return redirect(url_for('index')) #redirects to the search functionality
+
+if __name__ == '__main__':
+    app.run(debug=True)
